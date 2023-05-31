6ac8185606f50bd3fed4d2fd73d7d9b8748b2321
Aline Alves <alinealvesgoncalves1@gmail.com>
2023-05-31 16:53:59 -0300

    alterações

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,9 +1,11 @@
 # type: ignore
 from django.shortcuts import render
+
 from contact.models import Contact
 
+
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
 
     context = {
         'contacts': contacts,
@@ -14,3 +16,18 @@ def index(request):
         'contact/index.html',
         context
     )
+
+
+def contact(request, contact_id):
+    single_contact = Contact.objects.get(pk=contact_id)
+
+    context = {
+        'contact': single_contact,
+    }
+    
+    return render(
+        request,
+        'contact/contact.html',
+        context
+    )
+
