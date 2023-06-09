0b9efe1255da3a81fb3963a70c2ee8db5551b08f
Aline Alves <alinealvesgoncalves1@gmail.com>
2023-06-09 17:26:27 -0300

    alterações no forms, create e contact_forms

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,29 +1,27 @@
-from django.core.paginator import Paginator 
-from django.db.models import Q
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import render
 
+from contact.forms import ContactForm
 
-
-from contact.models import Contact
-
+  
 
 def create(request):
     if request.method == 'POST':
-        print()
-        print(request.method)
-        print(request.POST.get('first_name'))
-        print(request.POST.get('last_name'))
-        print()
+    
+        context = {
+            'form': ContactForm(request.POST)
+        }            
+    
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
 
     context = {
+        'form': ContactForm()
 
 
-    }
-
-    print()
-    print(request.method)
-    print()
-        
+    }            
     
     return render(
         request,
