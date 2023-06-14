4aec7be909c3aef8e82d1887a2a3d3050a919432
Aline Alves <alinealvesgoncalves1@gmail.com>
2023-06-14 16:26:10 -0300

    alterações

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,23 +1,19 @@
 from django.contrib import messages
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from contact.forms import RegisterForm
 
 
 def register(request):
     form = RegisterForm()
-
-    messages.info(request, 'Um texto qualquer')
-    messages.success(request, 'Um texto qualquer')
-    messages.warning(request, 'Um texto qualquer')
-    messages.error(request, 'Um texto qualquer')
-    
-
+      
     if request.method == 'POST':
         form = RegisterForm(request.POST)
 
         if form.is_valid():
-            form.save()
+           form.save()
+           messages.success(request, 'Usuário registrado')
+           return redirect('contact:index')
     
 
     return render(
